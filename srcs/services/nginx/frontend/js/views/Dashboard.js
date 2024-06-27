3cd5d90f72cbf497d3fa7f3bd5814325a36049f1
lkoletzk <lkoletzk@student.42.fr>
2024-06-27 15:35:07 +0200

    Continue front

--- srcs/services/nginx/frontend/js/views/Dashboard.js
+++ srcs/services/nginx/frontend/js/views/Dashboard.js
@@ -8,17 +8,26 @@ export default class extends AbstractView {
 
 	async getHtml() {
 		return `
-			<h1>Welcome back, Dom!</h1>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
-				odio sit amet nulla faucibus fringilla. Nullam quis ante sit amet
-				lorem ultricies tincidunt. Nullam nec odio sit amet nulla faucibus
-				fringilla. Nullam quis ante sit amet lorem ultricies tincidunt.
-			</p>
-			<p>
-				<a href="/posts" data-link>View recent posts</a>.
-			</p>
+			<div id ="dashboard">
+				<h1 data-i18n="welcome">Welcome, Dom!</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+					odio sit amet nulla faucibus fringilla. Nullam quis ante sit amet
+					lorem ultricies tincidunt. Nullam nec odio sit amet nulla faucibus
+					fringilla. Nullam quis ante sit amet lorem ultricies tincidunt.
+				</p>
+				<p>
+					<a href="/pong" data-link>Play Pong</a>.
+				</p>
+			</div>
 		`;
 	}
 }
 
+{/* <div id="language">
+<select id="language-selector">
+	<option value="en">🇬🇧&emsp;EN</option>
+	<option value="fr">🇫🇷&emsp;FR</option>
+	<option value="es">🇪🇸&emsp;ES</option>
+</select>
+</div> */}
--- srcs/services/nginx/frontend/js/views/Settings.js
+++ srcs/services/nginx/frontend/js/views/Settings.js
@@ -8,8 +8,8 @@ export default class extends AbstractView {
 
 	async getHtml() {
 		return `
-			<h1>Settings</h1>
-			<p>Manage your privacy and configuration settings.</p>
+			<h1 data-i18n="settings">Settings</h1>
+			<p data-i18n="settings_txt">Manage your privacy and configuration settings.</p>
 		`;
 	}
 }
