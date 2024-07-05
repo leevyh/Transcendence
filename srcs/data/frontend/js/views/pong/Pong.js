4a9b678c6b80ab3df87d1d2b8e22b05606af4791
Livia KOLETZKI <lkoletzk@made-f0Cr7s5.clusters.42paris.fr>
2024-07-05 18:38:51 +0200

    working Pong with little fixes needed

--- srcs/data/frontend/js/views/pong/Pong.js
+++ srcs/data/frontend/js/views/pong/Pong.js
@@ -1,5 +1,6 @@
 import AbstractView from "../AbstractView.js";
 
+
 export default class extends AbstractView {
 	constructor() {
 		super();
@@ -7,14 +8,14 @@ export default class extends AbstractView {
 	}
 
 	async getHtml() {
-		console.log("getHtml() called from game.js");
 		return `
 			<h1>Pong</h1>
-			<p data-i18n="pong">There will be a Pong Game here, soon...</p>
-			<div style="display: flex; justify-content: center; align-items: center;">
-				<canvas id="terrain" width="700" height="400" style="background: rgb(0, 0, 0);"></canvas>
+			<script type="module" src="Pong.js"></script>
+			<div id="bloc" style="display: flex;flex-direction: column;">
+				<!-- <div id="menu"><input id="startGame" style="width:120px" type="image" src="./img/startBtn.png"></div> -->
+				<div id="menu" style="display: flex;justify-content: center;padding: 10px;"><input id="startGame" type="button" value="Start Game" stye="justify-content: center;"></div>
+				<div id="divGame" style="display: flex;justify-content: center;/* width: 1000px; */background-color: azure;/* height: 1000px; */"><canvas id="GROUND" width="700" height="400" style="background: rgb(0, 0, 0); z-index: 0; position: absolute; left: auto; top: auto;"></canvas><canvas id="SCORE" width="700" height="400" style="z-index: 1; position: absolute; left: auto; top: auto;"></canvas><canvas id="JOUEURSETBALLE" width="700" height="400" style="z-index: 2; position: absolute; left: auto; top: auto;"></canvas></div>
 			</div>
-
 		`;
 	}
 }
