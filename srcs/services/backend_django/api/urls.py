d51c99d1c7793b06f875fbe664ae279d02f9cf25
Neoblacks <antoine.mugnier035@gmail.com>
2024-07-10 15:44:36 +0200

    Settings table added with view

--- srcs/services/backend_django/api/urls.py
+++ srcs/services/backend_django/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	# path('', views.index, name='index'),
 	path('api/register/', views.register, name='register'),
+	path('api/updateSettings/<int:user_id>', views.updateSettings, name='updateSettings'),
 ]
--- srcs/services/backend_django/api/views.py
+++ srcs/services/backend_django/api/views.py
@@ -4,7 +4,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.contrib.auth.hashers import make_password
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, SettingsUpdateForm
+from .models import User_site, Settings_user
 import json
 
 @csrf_exempt
@@ -17,6 +18,8 @@ def register(request):
 				user = form.save(commit=False)
 				user.password = make_password(form.cleaned_data['password'])
 				user.save()
+				settings = Settings_user(user=user)
+				settings.save()
 				return JsonResponse({'message': 'User registered successfully'}, status=201)
 			else:
 				return JsonResponse({'errors': form.errors}, status=400)
@@ -25,3 +28,21 @@ def register(request):
 	else:
 		return JsonResponse({'error': 'Invalid request method'}, status=405)
 
+
+@csrf_exempt
+def	updateSettings(request, user_id):
+	if request.method == 'POST':
+		try:
+			data = json.loads(request.body)
+			settings_id = Settings_user.objects.get(user=user_id)
+			form = SettingsUpdateForm(data, instance=settings_id)
+			if form.is_valid():
+				update_settings = form.save(commit=False)
+				update_settings.save()
+				return JsonResponse({'message': 'Settings updated successfully'}, status=200)
+			else:
+				return JsonResponse({'errors': form.errors}, status=400)
+		except:
+			return JsonResponse({'error': 'Invalid JSON'}, status=400)
+	else:
+		return JsonResponse({'error': 'Invalid request method'}, status=405)
