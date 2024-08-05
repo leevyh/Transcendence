3f27566c821a5cc4864b007831353570e8f56598
Livia KOLETZKI <lkoletzk@paul-f4Ar6s7.clusters.42paris.fr>
2024-08-05 14:07:59 +0200

    rollback chat

--- srcs/services/backend_django/chat/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Message
-
-class MessageSerializer(serializers.ModelSerializer):
-	timestamp = serializers.DateTimeField(read_only=True)
-	class Meta:
-		model = Message
-		fields = ['message', 'user_id', 'timestamp', 'room_name']
