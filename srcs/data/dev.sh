1091cecaddab53e25a7896147491ac869f3eda6f
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-09-03 18:46:24 +0200

    Update dev.sh to remove and create fleet folder

--- srcs/data/dev.sh
+++ srcs/data/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf ./certs ./ea/proxy-nginx ./ea ./elastic ./kibana ./logs || return 1
+rm -rf ./certs ./ea/proxy-nginx ./ea ./elastic ./kibana ./fleet ./logs || return 1
 
-mkdir -p ./certs ./ea ./ea/proxy-nginx ./elastic ./kibana ./logs || return 1
+mkdir -p ./certs ./ea ./ea/proxy-nginx ./elastic ./kibana ./fleet ./logs || return 1
