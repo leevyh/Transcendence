6114dcf932825a843768a5e4d7301ae0dfd2568f
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-09-06 16:57:35 +0200

    Make the two nginx server work with integrated Elastic Agent

--- srcs/data/dev.sh
+++ srcs/data/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf ./certs ./ea/proxy-nginx ./ea ./elastic ./fleet ./kibana ./logs
+rm -rf ./certs ./ea/proxy-nginx ./ea/frontend-nginx ./ea ./elastic ./fleet ./kibana ./logs
 
-mkdir -p ./certs ./ea ./ea/proxy-nginx ./elastic ./fleet ./kibana ./logs
+mkdir -p ./certs ./ea ./ea/proxy-nginx ./ea/frontend-nginx ./elastic ./fleet ./kibana ./logs
