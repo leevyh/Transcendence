f9ed76561d101ed84c4fd3414959b4ca89e3bdd4
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-09-24 18:42:37 +0200

    Fix rebase (routing)

--- srcs/data/backend/backend/routing.py
+++ srcs/data/backend/backend/routing.py
@@ -5,5 +5,5 @@ from chat import consumers as chat_consumers
 websocket_urlpatterns = [
     re_path(r'wss/status/$', consumers.StatusConsumer.as_asgi()),
     re_path(r'wss/friend_request/$', consumers.NotificationConsumer.as_asgi()),
-    re_path(r'wss/(?P<conversationID>\w+)/messages/$', chat_consumers.ChatMessagesConsumer.as_asgi()),
+    re_path(r'wss/(?P<conversationID>\w+)/messages/$', chat_consumers.ChatConsumer.as_asgi()),
 ]
