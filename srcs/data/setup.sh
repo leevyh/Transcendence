e9abca14db8ae5583e2737ea8b5b1cf8f4feb3bd
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-09-27 18:02:56 +0200

    Remove old data management scripts

--- srcs/data/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./frontend ./database ./database/data ./database/logs ./backend ./backend_/logs ./elastic ./elastic/ea ./elastic/elastic ./elastic/kibana ./elastic/logstash ./elastic/fleet ./elastic/certs ./elastic/logs ./elastic/ea/proxy-nginx ./elastic/ea/frontend-nginx
