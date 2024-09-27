1e506a4625e3eadd64a7b21b2a08e0a7f1241b75
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-09-27 17:50:10 +0200

    Add scripts for Makefile

---
+++ srcs/setup/dir_cleanup_force.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "x$1" = "x" ]; then
+    echo "Path not specified"
+    exit 1
+else
+  docker run --rm --name dir_cleanup_force -u 0 -v "$(pwd)/$1":/data debian:latest sh -c "rm -rf /data/backend/logs /data/database/logs /data/elastic /data/grafana /data/prometheus"
+fi
---
+++ srcs/setup/env_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "x$1" = "x" ]; then
+    echo "Path not specified"
+    exit 1
+fi
+
+if [ ! -f "$1/42api.env" ]; then
+    echo "42api.env file not found"
+    exit 1
+elif [ ! -f "$1/elastic.env" ]; then
+    echo "elastic.env file not found"
+    exit 1
+elif [ ! -f "$1/fleet.env" ]; then
+    echo "fleet.env file not found"
+    exit 1
+elif [ ! -f "$1/kibana.env" ]; then
+    echo "kibana.env file not found"
+    exit 1
+elif [ ! -f "$1/postgresql.env" ]; then
+    echo "postgresql.env file not found"
+    exit 1
+fi
