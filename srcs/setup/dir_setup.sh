dc63a35a97e0300493813d683f3ced4b7a074ec5
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-09-29 15:08:00 +0200

    Add directories to the setup script

--- srcs/setup/dir_setup.sh
+++ srcs/setup/dir_setup.sh
@@ -8,7 +8,7 @@ else
   mkdir -p "$1/database" "$1/database/data" "$1/database/logs"
   mkdir -p "$1/elastic" "$1/elastic/ea" "$1/elastic/elastic" "$1/elastic/kibana" "$1/elastic/logstash" "$1/elastic/fleet" "$1/elastic/certs" "$1/elastic/logs" "$1/elastic/ea/proxy-nginx" "$1/elastic/ea/frontend-nginx"
   mkdir -p "$1/frontend"
-  mkdir -p "$1/prometheus" "$1/prometheus/data" "$1/prometheus/logs"
+  mkdir -p "$1/prometheus" "$1/prometheus/data" "$1/prometheus/logs" "$1/prometheus/certs"
   mkdir -p "$1/grafana"
   mkdir -p "$1/alertmanager"
 fi
