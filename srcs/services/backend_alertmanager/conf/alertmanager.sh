aef9185faf477afb480ab998c4c31e3ee3124f76
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-09-29 17:16:58 +0200

    Configure Alertmanager

--- srcs/services/backend_alertmanager/conf/alertmanager.sh
+++ srcs/services/backend_alertmanager/conf/alertmanager.sh
@@ -2,5 +2,6 @@
 
 sed -i "s|ALERT_PASSWORD|$ALERT_PASSWORD|g" /config/alertmanager.yml
 sed -i "s|ALERT_EMAIL|$ALERT_EMAIL|g" /config/alertmanager.yml
+sed -i "s|ALERT_RECV|$ALERT_RECV|g" /config/alertmanager.yml
 
 /bin/alertmanager --config.file=/config/alertmanager.yml --storage.path=/alertmanager
