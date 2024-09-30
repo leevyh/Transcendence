1807147c83f5e33e6e9f7895cd8ddf2c230b181e
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-09-30 19:02:46 +0200

    Add ssl to grafana

--- srcs/services/setup_prometheus/conf/setup.sh
+++ srcs/services/setup_prometheus/conf/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ ! -f /pga/certs/ssl.crt ] && [ ! -f /pga/certs/ssl.key ]; then
+if [ ! -f /pga/certs/ssl.done ]; then
   echo "Creating certs";
-  openssl req -x509 -nodes -out /pga/certs/ssl.crt -keyout /pga/certs/ssl.key -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=localhost";
+  openssl req -x509 -nodes -out /pga/certs/prometheus.crt -keyout /pga/certs/prometheus.key -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=localhost";
+  openssl req -x509 -nodes -out /pga/certs/grafana.crt -keyout /pga/certs/grafana.key -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=localhost";
+  touch /pga/certs/ssl.done;
 fi;
 
 until false; do
