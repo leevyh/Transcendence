3ddeaa02f68eb6b3bc16438c5422c1019a7c44b9
Neoblacks <antoine.mugnier035@gmail.com>
2024-10-03 18:00:11 +0200

    FIX REDIRECTION WITH LOGIN 42
    Edit views.py to change user : user.nickname to nickname: user.nickname

--- srcs/data/frontend/js/views/callback42.js
+++ srcs/data/frontend/js/views/callback42.js
@@ -20,7 +20,7 @@ export function callback42(container) {
             if (data) {
                 if (DEBUG) {console.log(data);}
                 localStorage.setItem('token', data.token);
-                // navigateTo(`/user/${data.nickname}`);
+                navigateTo(`/user/${data.nickname}`);
             }
             else {
                 if (DEBUG) {console.log('No data');}
