151296fbbfa5db0630b9adbfe893a408586940c7
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-10-04 15:53:50 +0200

    Relative modifications of a new volume

--- srcs/setup/dir_cleanup_force.sh
+++ srcs/setup/dir_cleanup_force.sh
@@ -4,5 +4,5 @@ if [ "x$1" = "x" ]; then
     echo "Path not specified"
     exit 1
 else
-  docker run --rm --name dir_cleanup_force -u 0 -v "$(pwd)/$1":/data debian:latest sh -c "rm -rf /data/alertmanager /data/backend/logs /data/database/logs /data/elastic /data/grafana /data/prometheus"
+  docker run --rm --name dir_cleanup_force -u 0 -v "$(pwd)/$1":/data debian:latest sh -c "rm -rf /data/alertmanager /data/backend/certs /data/backend/logs /data/database/logs /data/elastic /data/grafana /data/prometheus"
 fi
