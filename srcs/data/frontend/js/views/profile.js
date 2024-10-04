7b9e357935908a49683771fe9e467070c75938c1
Lara Zanetti <larazanetti207@gmail.com>
2024-10-04 16:46:57 +0200

    modal avatar add + submid avatar back prblm

--- srcs/data/frontend/js/views/profile.js
+++ srcs/data/frontend/js/views/profile.js
@@ -1,7 +1,7 @@
 import { DEBUG, navigateTo } from '../app.js';
 import { getCookie } from './utils.js';
 import { navigationBar } from './navigation.js';
-import { createmainDiv, createUserCard } from '../component/chat/visual.js';
+import { DEBUG } from '../app.js';
 
 export function profileView(container) {
 
@@ -14,22 +14,10 @@ export function profileView(container) {
     const navBarContainer = navigationBar(container);
     div.appendChild(navBarContainer);
 
-    const mainDiv = document.createElement('div');
-    mainDiv.className = 'h-100 w-100 d-flex justify-content-center overflow-auto bg-light-subtle';
-    container.appendChild(mainDiv);
-
-    const separator = document.createElement('div');
-    separator.className = 'row h-100 w-75 bg-warning';
-    mainDiv.appendChild(separator);
-
-    const usersContainer = document.createElement('div');
-    usersContainer.className = 'bg-success';
-    separator.appendChild(usersContainer);
-
-    const chatContainer = document.createElement('div');
-    chatContainer.className = 'bg-danger';
-    separator.appendChild(chatContainer);
+    const viewContainer = document.createElement('div');
+    div.appendChild(viewContainer);
 
 
+    if (DEBUG) {console.log('Profile view for user:', userID);}
 
 }
