cc3f6974361972c47b3c4114d3f28811591cfcfa
Armand BINET <abinet@paul-f4Br6s7.clusters.42paris.fr>
2024-11-05 18:17:33 +0100

    add test API

--- srcs/data/backend/data/pong/urls.py
+++ srcs/data/backend/data/pong/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api/pong/start/', views.StartGameView.as_view(), name='start_game'),
     path('api/pong/move/', views.MovePlayerView.as_view(), name='move_player'),
     path('api/pong/stop/', views.StopGameView.as_view(), name='stop_game'),
+    path('test/', views.TestView.as_view(), name='test_view'),
 ]
--- srcs/data/backend/data/pong/views.py
+++ srcs/data/backend/data/pong/views.py
@@ -18,26 +18,24 @@ def pongSolo_view(request):
     # Retourner une page de jeu Pong en solo
     return render(request, 'index.html')
 
-# Vues d'API pour interagir avec le jeu via des requêtes HTTP
 class StartGameView(APIView):
-    async def post(self, request):
-        #appeler pong_view ou Po
+    def post(self, request):
+        # Logique pour démarrer le jeu
         return Response({"message": "Game started"}, status=status.HTTP_200_OK)
-        return redirect('pong_view')
 
 class MovePlayerView(APIView):
-    async def post(self, request):
-        player = request.data.get("player")
-        move = request.data.get("move")
-
-        # Appelle la méthode du consumer pour déplacer le joueur
-        await PongConsumer.cli_move_player(player, move)
-
-        return Response({"message": f"Player {player} moved {move}"}, status=status.HTTP_200_OK)
+    def post(self, request):
+        # Logique pour déplacer un joueur
+        return Response({"message": "Player moved"}, status=status.HTTP_200_OK)
 
 class StopGameView(APIView):
-    async def post(self, request):
-        # Appelle la méthode pour arrêter le jeu
-        await PongConsumer.cli_stop_game()
-
+    def post(self, request):
+        # Logique pour arrêter le jeu
         return Response({"message": "Game stopped"}, status=status.HTTP_200_OK)
+
+class TestView(APIView):
+    def get(self, request):
+        return Response({"message": "Test endpoint working!"}, status=status.HTTP_200_OK)
+        
+     def post(self, request):
+        return Response({"message": "Post request received!"}, status=status.HTTP_200_OK)
