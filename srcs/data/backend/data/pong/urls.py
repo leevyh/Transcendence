c02e0986f0bd24dbb99075aa4a2e3be177a73fdb
Armand BINET <abinet@paul-f4Br6s7.clusters.42paris.fr>
2024-11-05 15:08:40 +0100

    add views pour api

--- srcs/data/backend/data/pong/urls.py
+++ srcs/data/backend/data/pong/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('pong/', views.pong_view, name='pong'),
     path('tournament/', views.tournament_view, name='tournament'),
     path('pongSolo/', views.pongSolo_view, name='pongSolo'),
+    path('api/pong/start/', views.StartGameView.as_view(), name='start_game'),
+    path('api/pong/move/', views.MovePlayerView.as_view(), name='move_player'),
+    path('api/pong/stop/', views.StopGameView.as_view(), name='stop_game'),
 ]
--- srcs/data/backend/data/pong/views.py
+++ srcs/data/backend/data/pong/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from pong.classGame import PongGame  # Assurez-vous d'importer la classe appropriée
+import asyncio
 
+# Vues pour rendre les pages HTML
 def pong_view(request):
     # Retourner une page de jeu Pong
     return render(request, 'index.html')
@@ -11,3 +17,27 @@ def tournament_view(request):
 def pongSolo_view(request):
     # Retourner une page de jeu Pong en solo
     return render(request, 'index.html')
+
+# Vues d'API pour interagir avec le jeu via des requêtes HTTP
+class StartGameView(APIView):
+    async def post(self, request):
+        #appeler pong_view ou Po
+        return Response({"message": "Game started"}, status=status.HTTP_200_OK)
+        return redirect('pong_view')
+
+class MovePlayerView(APIView):
+    async def post(self, request):
+        player = request.data.get("player")
+        move = request.data.get("move")
+
+        # Appelle la méthode du consumer pour déplacer le joueur
+        await PongConsumer.cli_move_player(player, move)
+
+        return Response({"message": f"Player {player} moved {move}"}, status=status.HTTP_200_OK)
+
+class StopGameView(APIView):
+    async def post(self, request):
+        # Appelle la méthode pour arrêter le jeu
+        await PongConsumer.cli_stop_game()
+
+        return Response({"message": "Game stopped"}, status=status.HTTP_200_OK)
