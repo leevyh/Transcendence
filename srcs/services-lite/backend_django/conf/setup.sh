7090872ebd5af0f54e38ff1ba468e9551b6c373a
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-11-07 09:51:08 +0100

    Remove lite version

--- srcs/services-lite/backend_django/conf/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /var/app/ft_transcendence || exit 1
-
-python manage.py makemigrations
-python manage.py migrate
-
-daphne -e ssl:port=8000:privateKey=/etc/nginx/ssl/ssl.key:certKey=/etc/nginx/ssl/ssl.crt backend.asgi:application
