f2aa6d9b240521d211b981e1578883d10dadb532
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-11-07 13:05:11 +0100

    Django error handling

--- srcs/services/backend_django/conf/setup.sh
+++ srcs/services/backend_django/conf/setup.sh
@@ -6,4 +6,4 @@ python manage.py makemigrations
 #python manage.py loaddata /var/app/tools/data.json
 python manage.py migrate
 
-daphne -e ssl:port=8000:privateKey=/etc/nginx/ssl/ssl.key:certKey=/etc/nginx/ssl/ssl.crt backend.asgi:application > /var/log/django/django.log
+daphne -e ssl:port=8000:privateKey=/etc/nginx/ssl/ssl.key:certKey=/etc/nginx/ssl/ssl.crt backend.asgi:application 2>/var/log/django/e_django.log 1>/var/log/django/django.log
